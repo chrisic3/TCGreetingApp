0a02de782f94f778af961a84f02634c330275f2f
chrisic3 <chris_ic3@hotmail.com>
2026-01-22 21:09:02 -0600

    Update to .NET 6 and add interactive greeting app
    Changed target framework from .NET 8.0 to .NET 6.0. Replaced default console output with an interactive greeting application that prompts for the user's name and provides a personalized message.

--- GreetingApp/Program.cs
+++ GreetingApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to The Greeting Application!");
+Console.WriteLine("We are glad you are here.");
+Console.WriteLine("------------------------------------");
+Console.WriteLine();
+
+Console.Write("What is your name: ");
+string? userName = Console.ReadLine();
+Console.WriteLine();
+
+Console.WriteLine($"Hello! It's a pleasure to meet you, { userName }.");
+Console.WriteLine();
+
+Console.WriteLine("Thank you for using The Greeting Application. Goodbye.");
+Console.ReadLine();
